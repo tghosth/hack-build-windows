558e45ee165b59122c9cf741d7b5c529c050e0bb
Josh Grossman <tghosth@users.noreply.github.com>
2018-04-12 16:09:56 +0300

    Update hack-build-windows.ps1

--- hack-build-windows.ps1
+++ hack-build-windows.ps1
@@ -22,19 +22,12 @@ Set-CornerNavigationOptions -DisableUpperRightCornerShowCharms -DisableUpperLeft
 
 $cache = "$env:userprofile\AppData\Local\ChocoCache"
 New-Item -Path $cache -ItemType directory -Force
-$startPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PENTEST"
-
-if( -not (Test-Path -path $startPath) ) { New-Item -Path $startPath -ItemType directory }
-
-$desktopShortcut = Join-Path ${Env:USERPROFILE} "Desktop\PENTEST.lnk"
-
-Install-ChocolateyShortcut -shortcutFilePath $desktopShortcut -targetPath $startPath
 
 # Packages requiring reboot
 cinst powershell                    --cacheLocation $cache
 cinst dotnet4.7.1                   --cacheLocation $cache
 
-cinst jre8 --cacheLocation $cache
+
 cinst zap --cacheLocation $cache
 cinst nmap --cacheLocation $cache
 cinst winscp --cacheLocation $cache
@@ -49,3 +42,4 @@ cinst winmerge --cacheLocation $cache
 cinst wireshark --cacheLocation $cache
 cinst firefox --cacheLocation $cache
 cinst 7zip --cacheLocation $cache
+cinst jre8 --cacheLocation $cache
