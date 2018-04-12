eefc41463ae9eae5608cd065d46d23812801dbe3
Josh Grossman <tghosth@users.noreply.github.com>
2018-04-12 16:45:19 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,9 @@
 # To execute this script:
 # 1) Open powershell window as administrator
 # 2) Allow script execution by running command "Set-ExecutionPolicy Unrestricted"
-# 3) Execute the script by running ".\install.ps1"
+# 3) If necessary change to an appropriate directory
+# 4) If necessary, run Invoke-WebRequest -Uri https://raw.githubusercontent.com/tghosth/hack-build-windows/master/install.ps1 -OutFile install.ps1 to download this script.
+# 5) Execute the script by running ".\install.ps1"
 #
 # Based on https://github.com/fireeye/flare-vm/blob/master/install.ps1
 ###########################################
@@ -24,9 +26,9 @@ if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administ
   $cred=Get-Credential $env:username
 
   if ($cred) {
-      Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/tghosth/flare-vm/master/hack-build-windows.ps1 -Credential $cred
+      Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/tghosth/hack-build-windows/master/hack-build-windows.ps1 -Credential $cred
   } else {
-      Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/tghosth/flare-vm/master/hack-build-windows.ps1
+      Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/tghosth/hack-build-windows/master/hack-build-windows.ps1
   }
 
 } else {
